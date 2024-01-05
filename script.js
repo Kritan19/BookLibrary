94e883f54ee63d22da1692ce812f0ab82c12f47c
kritan <nepalkritan47@gmail.com>
2024-01-05 23:12:38 +0545

    added html , css and js file

---
+++ script.js
@@ -0,0 +1,12 @@
+const booksLibrary = [];
+
+function Book(title,pages,status,author){
+    this.title = title,
+    this.pages = status,
+    this.auther = author,
+    this.status = status;
+}
+
+function addBookToLibrary(){
+    
+}

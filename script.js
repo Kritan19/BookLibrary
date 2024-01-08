34551d1522abfd0d0eba6132eeda5d61385dbeb6
kritan <nepalkritan47@gmail.com>
2024-01-08 16:38:24 +0545

    added options in status

--- script.js
+++ script.js
@@ -12,22 +12,23 @@ function addBookToLibrary(event){
     event.preventDefault();
 
     //getting data that user inputed
-    var book_title = document.getElementById('title').value;
-    var book_pages = document.getElementById('pages').value;
-    var book_author = document.getElementById('author').value;
-    var book_status = document.getElementById('status').value;
+    var book_title = document.details.title.value;
+    var book_pages = document.details.pages.value;
+    var book_author= document.details.author.value;
+    var book_status= document.details.select_status.value;
+
+    console.log(book_title);
+    console.log(book_pages);
+    console.log(book_author);
+    console.log(book_status);
+
 
     //using the book constructor to store data in form of object;
     var formData = new book(book_title,book_pages,book_status,book_author);
 
     //using stack data structure to add form in the booksLibrary Array
     booksLibrary.push(formData);
-
-    //clearing the form .
-    document.getElementById('title').value ='';
-    document.getElementById('pages').value ='';
-    document.getElementById('author').value ='';
-    document.getElementById('status').value ='';
-
-
+    console.log("booksLibrary :",booksLibrary);
+    
 }
+
